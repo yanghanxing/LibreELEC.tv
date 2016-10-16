bdc7ee9545cd545daf8835ccbebe2c91e3de130b
MilhouseVH <milhouseVH.github@nmacleod.com>
2016-10-16 13:27:24 +0100

    openssh: UsePrivilegeSeparation/StrictModes not needed

--- packages/network/openssh/package.mk
+++ packages/network/openssh/package.mk
@@ -67,16 +67,17 @@ post_makeinstall_target() {
   mkdir -p $INSTALL/usr/share/services
     cp -P $PKG_DIR/default.d/*.conf $INSTALL/usr/share/services
 
-  sed -i $INSTALL/etc/ssh/sshd_config -e "s|^#PermitRootLogin.*|PermitRootLogin yes|g"
+  sed -e "s|^#PermitRootLogin.*|PermitRootLogin yes|g" \
+      -e "s|^#StrictModes.*|StrictModes no|g" \
+      -e "s|^#UsePrivilegeSeparation.*|UsePrivilegeSeparation no|g" \
+      -i $INSTALL/etc/ssh/sshd_config
+
   echo "PubkeyAcceptedKeyTypes +ssh-dss" >> $INSTALL/etc/ssh/sshd_config
 
   debug_strip $INSTALL/usr
 }
 
 post_install() {
-  add_user sshd x 74 74 "Privilege-separated SSH" "/var/empty/sshd" "/bin/sh"
-  add_group sshd 74
-
   enable_service sshd-defaults.service
   enable_service sshd.service
 }

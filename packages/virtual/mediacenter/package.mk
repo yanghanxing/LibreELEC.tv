461f3d38f83d3345439996ddf5e98b93365f3d00
Jonas Karlman <jonas@kwiboo.se>
2017-03-03 20:41:16 +0100

    new package: plexht

--- packages/virtual/mediacenter/package.mk
+++ packages/virtual/mediacenter/package.mk
@@ -46,3 +46,13 @@ if [ "$MEDIACENTER" = "kodi" ]; then
                                           xmlstarlet \
                                           peripheral.joystick"
 fi
+
+if [ "$MEDIACENTER" = "plexht" ]; then
+# some python stuff needed for various addons
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET Pillow \
+                                          simplejson \
+                                          pycrypto"
+# other packages
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET OpenPHT-settings \
+                                          xmlstarlet"
+fi

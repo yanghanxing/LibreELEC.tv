e1f24647014aa310cd9afed3f6d37216373574f6
Jonas Karlman <jonas@kwiboo.se>
2017-03-03 20:41:16 +0100

    libmodplug: use shared library

--- packages/audio/libmodplug/package.mk
+++ packages/audio/libmodplug/package.mk
@@ -29,5 +29,3 @@ PKG_LONGDESC="libmodplug renders mod music files as raw audio data, for playing 
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
-
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared"

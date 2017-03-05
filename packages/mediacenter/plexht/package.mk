bdf4904dda4ca6e872139bb88cc5b5853d08587b
Jonas Karlman <jonas@kwiboo.se>
2017-03-05 17:49:41 +0100

    tinkerboard: wip

--- packages/mediacenter/plexht/package.mk
+++ packages/mediacenter/plexht/package.mk
@@ -238,6 +238,27 @@ elif [ "$KODIPLAYER_DRIVER" = libamcodec ]; then
         -DCMAKE_INSTALL_PREFIX=/usr/lib/plexht \
         -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
         ..
+elif [ "$PROJECT" = TinkerBoard ]; then
+  export PYTHON_EXEC="$SYSROOT_PREFIX/usr/bin/python2.7"
+  cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
+        -DENABLE_PYTHON=ON \
+        -DEXTERNAL_PYTHON_HOME="$SYSROOT_PREFIX/usr" \
+        -DPYTHON_EXEC="$PYTHON_EXEC" \
+        -DSWIG_EXECUTABLE=`which swig` \
+        -DSWIG_DIR="$ROOT/$BUILD/toolchain" \
+        -DCMAKE_PREFIX_PATH="$SYSROOT_PREFIX" \
+        -DCMAKE_LIBRARY_PATH="$SYSROOT_PREFIX/usr/lib" \
+        -DCMAKE_INCLUDE_PATH="$SYSROOT_PREFIX/usr/include;$SYSROOT_PREFIX/usr/include/python2.7" \
+        -DCOMPRESS_TEXTURES=OFF \
+        -DENABLE_DUMP_SYMBOLS=ON \
+        -DENABLE_AUTOUPDATE=ON \
+        -DTARGET_PLATFORM=ROCKCHIP \
+        -DUSE_INTERNAL_FFMPEG=OFF \
+        -DOPENELEC=ON \
+        -DLIRC_DEVICE=/run/lirc/lircd \
+        -DCMAKE_INSTALL_PREFIX=/usr/lib/plexht \
+        -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
+        ..
 else
   export PYTHON_EXEC="$SYSROOT_PREFIX/usr/bin/python2.7"
   cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \

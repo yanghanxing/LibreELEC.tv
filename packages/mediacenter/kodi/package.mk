b21ab3875991232eab320d1b5c021b86285bedf8
Jonas Karlman <jonas@kwiboo.se>
2017-06-06 15:49:52 +0200

    kodi: use KODIPLAYER_DRIVER to enable rkmpp

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -37,13 +37,6 @@ PKG_CMAKE_SCRIPT="$ROOT/$PKG_BUILD/project/cmake/CMakeLists.txt"
 
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET dbus"
 
-if [ "$PROJECT" = "Rockchip" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libdrm"
-  KODI_GBM="-DENABLE_GBM=ON -DENABLE_RK=ON"
-else
-  KODI_GBM=""
-fi
-
 if [ "$DISPLAYSERVER" = "x11" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libX11 libXext libdrm libXrandr"
   KODI_XORG="-DENABLE_X11=ON"
@@ -205,6 +198,8 @@ if [ ! "$KODIPLAYER_DRIVER" = default ]; then
     CXXFLAGS="$CXXFLAGS -DHAS_IMXVPU -DLINUX -DEGL_API_FB"
   elif [ "$KODIPLAYER_DRIVER" = libamcodec ]; then
     KODI_PLAYER="-DENABLE_AML=ON"
+  elif [ "$KODIPLAYER_DRIVER" = rkmpp ]; then
+    KODI_PLAYER="-DENABLE_GBM=ON -DENABLE_RK=ON"
   fi
 fi
 
@@ -237,7 +232,6 @@ PKG_CMAKE_OPTS_TARGET="-DNATIVEPREFIX=$ROOT/$TOOLCHAIN \
                        $KODI_VAAPI \
                        $KODI_JOYSTICK \
                        $KODI_CEC \
-                       $KODI_GBM \
                        $KODI_XORG \
                        $KODI_SAMBA \
                        $KODI_NFS \
